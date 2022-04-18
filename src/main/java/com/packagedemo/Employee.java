015e7d96509ac59bb1b0aaaa459a1d7be5d604cf
saranamsairakesh <saranamsairakesh@gmail.com>
2022-04-18 11:08:00 +0530

    First Commit

---
+++ src/main/java/com/packagedemo/Employee.java
@@ -0,0 +1,16 @@
+package com.packagedemo;
+
+public class Employee {
+    private int id;
+    private int daysWorked;
+    private int salaryPerDay;
+    public Employee(int id, int daysWorked, int salaryPerDay) {
+        this.id = id;
+        this.daysWorked = daysWorked;
+        this.salaryPerDay = salaryPerDay;
+    }
+
+    private int getSalaryPerMonthy() {
+        return daysWorked*salaryPerDay;
+    }
+}
